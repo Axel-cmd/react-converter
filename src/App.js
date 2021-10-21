554ea44bed76f255e4b274076becc300d5f31a8d
Axel-cmd <axelminart.pro@gmail.com>
2021-10-21 19:27:23 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      
+      <p>test </p>
     </div>
   );
 }
