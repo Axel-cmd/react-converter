1af8e443922f9aea23a970567ae8224f501307cf
Axel-cmd <axelminart.pro@gmail.com>
2021-10-25 19:39:15 +0200

    1 file modified, 3 added, 3 deleted

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import CalculatorConverter from './CalculatorConverter';
+import CalculatorConverter from '../converter/CalculatorConverter';
 
 
 export default function App() {
