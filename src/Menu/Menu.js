00918269f1cf433bc0d5628c2ec79689b90b1491
Axel-cmd <axelminart.pro@gmail.com>
2021-10-25 23:23:16 +0200

    1 file modified

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { Button} from '@material-ui/core'
+import React from 'react';
+import { Button } from '@material-ui/core';
 
-export default class Menu extends React.Component{
-
-
-  constructor(props)
-  {
-    super(props)
-
+export default class Menu extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  onChange(value)
-  {
+  onChange(value) {
     this.props.onChangeValue(value);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-      <Button variant="contained" onClick={()=> this.onChange(true)}>Devise</Button>
-      <Button variant="contained" onClick={()=> this.onChange(false)}>Distance</Button>
+        <Button
+          style={{ marginLeft: '20%' }}
+          variant="contained"
+          onClick={() => this.onChange(true)}
+        >
+          Devise
+        </Button>
+        <Button
+          variant="contained"
+          style={{ marginLeft: '50%' }}
+          onClick={() => this.onChange(false)}
+        >
+          Distance
+        </Button>
       </div>
-    )
+    );
   }
-
 }
